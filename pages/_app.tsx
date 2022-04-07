eab7a04cccb9ed0be5a79f792b54078a77993e75
nerd <cirsten@wijvancees.nl>
2022-04-07 16:53:39 +0200

    Add Chakra UI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,26 @@
-import '../styles/globals.css'
+/* eslint-disable react/jsx-props-no-spreading */
+import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import Layout from '../lib/layout'
+import customTheme from '../lib/styles/customTheme'
+import '../lib/styles/globals.css'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return (
+    <ChakraProvider theme={customTheme}>
+      <Head>
+        <meta
+          name='viewport'
+          content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover'
+        />
+      </Head>
+
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
